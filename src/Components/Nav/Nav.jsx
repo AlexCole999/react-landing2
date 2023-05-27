c8cab54c4acb8f3da9e7831316ed55bae6397b2c
Leo <62186360+AlexCole999@users.noreply.github.com>
2023-05-27 20:27:16 +0500

    Nav component === complete 390px

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,22 @@
 import './Nav.scss'
+import Logo from '../../assets/Logo.svg'
+import header from '../../assets/header-burgerButton.svg'
 
 function Nav() {
 
   return (
-    <div className="Nav">
-      1111
+    <div className='Nav'>
+
+      <img className='Nav__logo' src={Logo} alt="" />
+
+      <div className="Nav__menu">
+        <div className="Nav__menuelem">About</div>
+        <div className="Nav__menuelem">Products</div>
+        <div className="Nav__menuelem">Recipes</div>
+      </div>
+
+      <img src={header} alt="" />
+
     </div>
   )
 }
