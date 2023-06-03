502240303a789a6207d2714fbfc14f99e5859cce
Leo <62186360+AlexCole999@users.noreply.github.com>
2023-06-03 16:55:48 +0500

    FreshBakery desktop DONE

--- src/Components/FreshBakery/FreshBakery.jsx
+++ src/Components/FreshBakery/FreshBakery.jsx
@@ -18,51 +18,55 @@ function FreshBakery() {
 
       <div className="FreshBakery">
 
-        <div className="FreshBakery__leftside">
-          <AnimationComponent
-            className={"FreshBakery__title"}
-            moveAnimation={{ left: { from: -25, to: 0 } }}
-            transition={1}
-            triggerOnce={true}
-            opacityAnimation={{ from: 0, to: 1 }}
-            blurAnimation={{ from: 10, to: 0 }}
-          >
-            Fresh Bakery Every Day
-          </AnimationComponent>
+        <div className="FreshBakery__container">
 
-          <AnimationComponent
-            className={"FreshBakery__text"}
-            moveAnimation={{ right: { from: -25, to: 0 } }}
-            transition={1}
-            triggerOnce={true}
-            opacityAnimation={{ from: 0, to: 1 }}
-            blurAnimation={{ from: 10, to: 0 }}
-          >
-            Even the all-powerful Pointing has no control about the blind texts it is an almost.
-          </AnimationComponent >
+          <div className="FreshBakery__leftside">
+            <AnimationComponent
+              className={"FreshBakery__title"}
+              moveAnimation={{ left: { from: -25, to: 0 } }}
+              transition={1}
+              triggerOnce={true}
+              opacityAnimation={{ from: 0, to: 1 }}
+              blurAnimation={{ from: 10, to: 0 }}
+            >
+              Fresh Bakery Every Day
+            </AnimationComponent>
 
-          <AnimationComponent
-            className={"FreshBakery__subtitle"}
-            moveAnimation={{ left: { from: -25, to: 0 } }}
-            transition={1}
-            delay={2500}
-            triggerOnce={true}
-            opacityAnimation={{ from: 0, to: 1 }}
-            blurAnimation={{ from: 10, to: 0 }}
-          >
-            Sience 1984
-          </AnimationComponent>
-        </div>
+            <AnimationComponent
+              className={"FreshBakery__text"}
+              moveAnimation={{ right: { from: -25, to: 0 } }}
+              transition={1}
+              triggerOnce={true}
+              opacityAnimation={{ from: 0, to: 1 }}
+              blurAnimation={{ from: 10, to: 0 }}
+            >
+              Even the all-powerful Pointing has no control about the blind texts it is an almost.
+            </AnimationComponent >
 
-        <div className="FreshBakery__img">
-          <AnimationComponent
-            transition={2}
-            triggerOnce={true}
-            opacityAnimation={{ from: 0.2, to: 1 }}
-            blurAnimation={{ from: 10, to: 0 }}
-          >
-            <img src={mainImg} alt="" />
-          </AnimationComponent>
+            <AnimationComponent
+              className={"FreshBakery__subtitle"}
+              moveAnimation={{ left: { from: -25, to: 0 } }}
+              transition={1}
+              delay={2500}
+              triggerOnce={true}
+              opacityAnimation={{ from: 0, to: 1 }}
+              blurAnimation={{ from: 10, to: 0 }}
+            >
+              Sience 1984
+            </AnimationComponent>
+          </div>
+
+          <div>
+            <AnimationComponent
+              transition={2}
+              triggerOnce={true}
+              opacityAnimation={{ from: 0.2, to: 1 }}
+              blurAnimation={{ from: 10, to: 0 }}
+            >
+              <img className="FreshBakery__img" src={mainImg} alt="" />
+            </AnimationComponent>
+          </div>
+
         </div>
 
       </div >
