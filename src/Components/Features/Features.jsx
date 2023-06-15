1ffe56bb07bb5fa845cc84d19254dff999828f23
Leo <62186360+AlexCole999@users.noreply.github.com>
2023-06-15 22:06:30 +0500

    appended rowelems in Features component

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,13 +1,8 @@
 import './Features.scss'
 import icon1 from '../../assets/icons/Features-icon1.svg'
-
-function FeaturesElem({ icon, title, text }) {
-  return (
-    <div className="Features__rowelem">
-      <img src={icon} fill='red' alt="" />
-    </div>
-  )
-}
+import icon2 from '../../assets/icons/Features-icon2.svg'
+import icon3 from '../../assets/icons/Features-icon3.svg'
+import elemPicture from '../../assets/icons/Features-elemPicture.webp'
 
 function Features() {
 
@@ -24,9 +19,32 @@ function Features() {
         </div>
 
         <div className="Features__row">
-          <FeaturesElem icon={icon1} />
-          <FeaturesElem />
-          <FeaturesElem />
+
+          <div className="Features__rowelem">
+            <div className="Features__rowelemcontent">
+              <img className="Features__rowelemicon" src={icon1} alt="" />
+              <div className="Features__rowelemtitle">Fresh</div>
+              <div className="Features__rowelemtext">Nothing the copy said could convince her and so it didn’t take long.</div>
+            </div>
+          </div>
+
+          <div className="Features__rowelem">
+            <div className="Features__rowelemcontent">
+              <img className="Features__rowelemicon" src={icon2} alt="" />
+              <div className="Features__rowelemtitle">Natural</div>
+              <div className="Features__rowelemtext">Copy Writers ambushed her, made her drunk with Longe and Parole.</div>
+            </div>
+          </div>
+
+          <div className="Features__rowelem" style={{ backgroundColor: '#F9BF77' }}>
+            <div className="Features__rowelemcontent">
+              <img className="Features__rowelemicon" src={icon3} alt="" />
+              <div className="Features__rowelemtitle">Tasty</div>
+              <div className="Features__rowelemtext">And if she hasn’t been rewritten, then they are still using her.</div>
+              <img className="Features__rowelempicture" src={elemPicture} alt="" />
+            </div>
+          </div>
+
         </div>
 
       </div>
