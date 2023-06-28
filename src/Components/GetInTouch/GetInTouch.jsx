c176239f7594a2b787d1e8cfdf7e3fc5549353f4
Leo <62186360+AlexCole999@users.noreply.github.com>
2023-06-28 21:19:50 +0500

    continued GetInTouch

--- src/Components/GetInTouch/GetInTouch.jsx
+++ src/Components/GetInTouch/GetInTouch.jsx
@@ -1,5 +1,6 @@
 import './GetInTouch.scss'
 import AnimationComponent from './../AnimationComponent/AnimationComponent';
+import img from '../../assets/img/GetInTouch-img.webp'
 import imgzipped from '../../assets/img/GetInTouch-imgzipped.webp'
 import bgimg1 from '../../assets/img/GetInTouch-bgimg1.webp'
 import AddressIcon from '../../assets/icons/GetInTouch-icon1.svg'
@@ -30,6 +31,7 @@ function GetInTouch() {
           <AnimationComponent
             className={"GetInTouch__title"}
             transition={1.3}
+            threshold={0.65}
             triggerOnce={true}
             moveAnimation={{ bottom: { from: 25, to: 0 } }}
             opacityAnimation={{ from: 0.01, to: 1 }}
@@ -39,10 +41,10 @@ function GetInTouch() {
             </div>
           </AnimationComponent>
 
-
           <AnimationComponent
             className={"GetInTouch__subtitle"}
             transitionDelay={0.8}
+
             transition={1.3}
             triggerOnce={true}
             moveAnimation={{ bottom: { from: 25, to: 0 } }}
@@ -53,21 +55,51 @@ function GetInTouch() {
             </div>
           </AnimationComponent>
 
-          <div className="GetInTouch__contacts">
-            <ContactElem
-              icon={AddressIcon}
-              counter={'01'}
-              title={'Address'}
-              text={'764 Richardson Rd. Bethpage, NY 11714'}
-            />
-            <ContactElem
-              icon={PhoneIcon}
-              counter={'02'}
-              title={'Phone'}
-              text={'+1 (234) 567-89-02'}
-            />
-          </div>
+          <AnimationComponent
+            className={"GetInTouch__img_mobile"}
+            transition={1.3}
+            threshold={0.85}
+            triggerOnce={true}
+            moveAnimation={{ bottom: { from: 25, to: 0 } }}
+            opacityAnimation={{ from: 0.01, to: 1 }}
+            preloadComponent={imgzipped}
+          >
+            <img src={img} alt="" />
+          </AnimationComponent>
 
+          <div className="GetInTouch__contacts">
+            <AnimationComponent
+              className={"GetInTouch__contactselem"}
+              transition={1.3}
+              threshold={0.65}
+              triggerOnce={true}
+              moveAnimation={{ left: { from: 25, to: 0 } }}
+              opacityAnimation={{ from: 0.01, to: 1 }}
+            >
+              <ContactElem
+                icon={AddressIcon}
+                counter={'01'}
+                title={'Address'}
+                text={'764 Richardson Rd. Bethpage, NY 11714'}
+              />
+            </AnimationComponent>
+            <AnimationComponent
+              className={"GetInTouch__contactselem"}
+              transition={1.3}
+              transitionDelay={0.8}
+              threshold={0.65}
+              triggerOnce={true}
+              moveAnimation={{ left: { from: 25, to: 0 } }}
+              opacityAnimation={{ from: 0.01, to: 1 }}
+            >
+              <ContactElem
+                icon={PhoneIcon}
+                counter={'02'}
+                title={'Phone'}
+                text={'+1 (234) 567-89-02'}
+              />
+            </AnimationComponent>
+          </div>
 
         </div>
 
