65a5bf8e636a497b4fb214f84e225e57ecbe1e17
AlexCole999 <AlexCole999@users.noreply.github.com>
2023-07-11 16:50:06 +0000

    deploy: eddfe7a701feeebc26bdc1b7c47ad36574e2b15e

---
+++ assets/Recipes-a98922cc.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-43473571.js";function c(){return e("div",{className:"Recipes",children:"Recipes"})}export{c as default};
--- assets/Recipes-e9fba1ed.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-d12f74b9.js";function c(){return e("div",{className:"Recipes",children:"Recipes"})}export{c as default};
